3fc6bc6f6751a4472b74b80f340f81a28941bb46
Djibba22 <johnwaynedinsmore@gmail.com>
2019-11-07 11:54:57 -0500

    added catch all route to server

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ app.use(flash());
 /* Serve up static assets (usually on heroku) */
 if (process.env.NODE_ENV === "production") {
   app.use(passport.session()); app.use(express.static(path.join(__dirname, '../build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 };
 
 /* === Routing === */
